98928ed6ca4e3ec33c76610003d2f830adc13fe1
Lucas Teixeira <ltx16@hotmail.com>
2018-02-12 21:08:16 -0200

    Teste de service worker

--- phone/serviceworker.js
+++ phone/serviceworker.js
@@ -7,23 +7,23 @@
 //Qualquer coisa, vem cá, Polícia do Apache.
 
 
-self.addEventListener('install', e => {
+self.addEventListener('fetch', e => {
   let timeStamp = Date.now();
   e.waitUntil(
     caches.open('YTPBR10').then(cache => {
       return cache.addAll([
-        `/`,
-        `/index.html?timestamp=${timeStamp}`,
-		`/css/site_global.css`,
-		`/phone/css/master_mestre3224.css?3908474420`,
-		`/phone/css/index.css?4217621930`,
-		`/images/u702-4.png`,
-		`/images/u1119-4.png`,
-		`/images/u1104-6.png`,
-		`/images/logo%2010anos%20ytpbr-crop-u675.png`,
-		`/images/u1122-4.png`,
+        "/",
+        "/index.html?timestamp=${timeStamp}",
+		"/css/site_global.css",
+		"/phone/css/master_mestre3224.css?3908474420",
+		"/phone/css/index.css?4217621930",
+		"/images/u702-4.png",
+		"/images/u1119-4.png",
+		"/images/u1104-6.png",
+		"/images/logo%2010anos%20ytpbr-crop-u675.png",
+		"/images/u1122-4.png",
 		/* ... */		
-		`/images/logo_ytpbr10_lowres.png`
+		"/images/logo_ytpbr10_lowres.png"
       ])
       .then(() => self.skipWaiting());
     })
